93c20f4efb9a19ad92a138d955d42f1001a62333
Roy Jiny <royjiny@gmail.com>
2020-12-05 16:03:30 +0200

    before Linux switch

--- assembler.c
+++ assembler.c
@@ -7,7 +7,7 @@
 #include "assembler.h"
 
 
-void first_run(FILE* proggram) 
+void first_run(FILE* program) 
 {
 	init_labels_array(); /*TODO: free somewhere*/
 	int PC = 0;
@@ -15,7 +15,7 @@ void first_run(FILE* proggram)
 	char line[MAX_LINE_SIZE];
 	char *label = (char*)malloc(MAX_LABEL_SIZE * sizeof(char));
 	char *addr  = (char*)malloc(ADDRESS_SIZE * sizeof(char));
-	while (fgets(line, MAX_LINE_SIZE, proggram) != NULL) /*get current line*/
+	while (fgets(line, MAX_LINE_SIZE, program) != NULL) /*get current line*/
 	{
 		lt = get_line_type(line);
 		if (lt == LABEL)
